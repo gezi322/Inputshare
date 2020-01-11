9ab811f62aae595c40762ad554be7149ac6c8f4e
sbarrac1 <sbarrac@outlook.com>
2020-01-11 11:26:11 +0000

    Catched errors on server start

--- Inputshare/Program.cs
+++ Inputshare/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Logging.Serilog;
 using Avalonia.ReactiveUI;
 using InputshareLib;
+using InputshareLib.Server;
 
 namespace Inputshare
 {
@@ -14,10 +16,24 @@ namespace Inputshare
             ISLogger.PrefixCaller = false;
             ISLogger.PrefixTime = true;
             ISLogger.EnableConsole = true;
-            ISLogger.EnableLogFile = false;
-            ISLogger.Write("Inputshare started");
+            ISLogger.EnableLogFile = true;
+            ISLogger.SetLogFileName("Inputshare.log");
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
             BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            ISLogger.Write("UNHANDLED EXCEPTION!");
+            Exception ex = e.ExceptionObject as Exception;
+            while (ex.InnerException != null)
+                ex = ex.InnerException;
+
+            ISLogger.Write(ex.Message);
+            ISLogger.Write(ex.Source);
+            ISLogger.Write(ex.StackTrace);
         }
 
         public static AppBuilder BuildAvaloniaApp()
